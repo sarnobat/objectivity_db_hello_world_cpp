21ed029e9656bbabc76858c845edd69ede9a7561
sarnobat <ss401533@gmail.com>
2013-09-18 17:20:39 -0700

    Initial commit

---
+++ Objectivity/11.1/setup.sh
@@ -0,0 +1,12 @@
+
+# appending /Applications/Objectivity/11.1/bin to PATH
+PATH=${PATH+"$PATH:"}/Applications/Objectivity/11.1/bin; export PATH
+
+# appending /Applications/Objectivity/11.1/lib to DYLD_LIBRARY_PATH
+DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH+"$DYLD_LIBRARY_PATH:"}/Applications/Objectivity/11.1/lib; export DYLD_LIBRARY_PATH
+
+# appending /Applications/Objectivity/11.1/assist to PATH
+PATH=${PATH+"$PATH:"}/Applications/Objectivity/11.1/assist; export PATH
+
+# appending /Applications/Objectivity/11.1/java/lib/oojava.jar to CLASSPATH
+CLASSPATH=${CLASSPATH+"$CLASSPATH:"}/Applications/Objectivity/11.1/java/lib/oojava.jar; export CLASSPATH

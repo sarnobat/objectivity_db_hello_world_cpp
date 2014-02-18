a7c3c7884df0ab0fa32b666e4920aed08f789bf2
sarnobat <ss401533@gmail.com>
2014-02-17 22:15:48 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ export DYLD_LIBRARY_PATH=$PWD/Objectivity/11.1/lib
 
 cd ./Objectivity/11.1/samples/cxx/helloWorld
 
+# you also need to put this in ooddlx at the beginning otherwise stdarg.h will not be found /usr/llvm-gcc-4.2/lib/gcc/i686-apple-darwin11/4.2.1/include
 make
 
 ./helloWorld
